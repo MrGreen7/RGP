5fd5637cffffa9476eeb1b4b8d39dd71f219b6ab
MrGreen7 <kadiidivide@gmail.com>
2017-12-28 17:32:37 +0100

    Project Anitialized

---
+++ U_Main.pas
@@ -0,0 +1,39 @@
+unit U_Main;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.TabControl,
+  FMX.StdCtrls, FMX.Controls.Presentation, FMX.Styles.Objects, FMX.Ani;
+
+type
+  TMain = class(TForm)
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    TabItem2: TTabItem;
+    TabControl2: TTabControl;
+    Medical: TTabItem;
+    Label3: TLabel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Main: TMain;
+
+implementation
+
+{$R *.fmx}
+
+end.
